c73bc5d954818ece35c87fb95f41ddfb4157ce56
RicardoMFC <rich10fernandezconde@gmail.com>
2023-04-11 13:50:27 +0200

    Update modeloAleman.py
    f

--- modeloAleman.py
+++ modeloAleman.py
@@ -1,4 +1,22 @@
-def aleman():
+def aleman():    
     V = 200000
-    i = 0.05
+    saldo_deudor=V
+    capital_amortizado=V/12
+
+    interes = 0.05
     n = 12
+    interes[1]=i*V
+ 
+
+    for i in range(n):        
+        cuota_interes=saldo_deudor*interes
+        saldo_deudor=saldo_deudor-capital_amortizado
+        cuota=cuota_interes + capital_amortizado
+        print(cuota, capital_amortizado, cuota_interes)
+        #print ("Cuota: {} Interes: {} Capital amortizado: {}\n".format{Cuota, cuota_interes, capital_amortizado})
+
+
+
+
+
+
