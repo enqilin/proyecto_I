56c63fb5c808f3c16b9b59b0440295c19176dee8
nad0us <113980750+nad0us@users.noreply.github.com>
2023-04-17 11:39:50 +0200

    Update modeloAmericano.py
    cambios

--- modeloAmericano.py
+++ modeloAmericano.py
@@ -59,3 +59,15 @@ class americano:
 amortizacion = americano(100000, 0.03, 5)
 
 amortizacion.imprimir_tabla_amortizacion()
+
+
+
+
+amortizacion = americano(100000, 0.025, 10)
+tabla_amortizacion = amortizacion.calcula_amortizacion()
+
+print("{:^10s} | {:^15s} | {:^15s} | {:^15s} | {:^15s}".format("Mes", "Cuota", "Interés", "Amortización", "Saldo de deuda"))
+print("-"*75)
+
+for fila in tabla_amortizacion:
+    print("{:^10d} | {:>15,.2f} | {:>15,.2f} | {:>15,.2f} | {:>15,.2f}".format(fila['mes'], fila['cuota'], fila['intereses_mes'], fila['capital_mes'], fila['saldo_pendiente']))
