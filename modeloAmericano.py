5161bec42a793fe5a3586a2ed2b225826ddb5b47
nad0us <113980750+nad0us@users.noreply.github.com>
2023-04-17 11:31:15 +0200

    Update modeloAmericano.py
    cambios

--- modeloAmericano.py
+++ modeloAmericano.py
@@ -10,38 +10,40 @@ class americano:
         cuota = 0
         interes_total = 0
         cuota_total = 0
+        capital_total = 0
         tabla_amortizacion =[]
         for mes in range(1 , self.plazo * 12 + 2):
-            interes_total = self.tasa / 12
             interes_mensual = (self.tasa /12)*self.prestamo
+            interes_total += interes_mensual
             amor_pendiente = self.prestamo
             if mes == self.plazo*12:
                 capital_mensual = self.tasa / 12 +self.prestamo
                 amor_pendiente-= capital_mensual
                 cuota = (self.tasa/ 12) *self.prestamo + self.prestamo
                 cuota_total+=cuota
+                capital_total+=capital_mensual
                 tabla_amortizacion.append({
                     "mes": mes,
                     "saldo_pendiente": amor_pendiente,
                     "capital_mes": capital_mensual,
                     "intereses_mes":interes_mensual,
                     "cuota" :cuota})
+                
             elif mes == self.plazo*12 +1:
                 capital_mensual = self.tasa / 12
                 cuota = (self.tasa/ 12) *self.prestamo
                 cuota_total+=cuota
-                interes_total+= self.tasa /12
                 tabla_amortizacion.append({
-                    "mes": False,
-                    "saldo_pendiente":False ,
-                    "capital_mes": False,
-                    "intereses_mes":False,
+                    "mes": mes,
+                    "saldo_pendiente":amor_pendiente ,
+                    "capital_mes": capital_total,
+                    "intereses_mes":interes_total,
                     "cuota" :cuota_total})
             else:
                 capital_mensual = self.tasa / 12
                 cuota = (self.tasa/ 12) *self.prestamo
-                interes_total+= self.tasa /12
                 cuota_total+=cuota
+                capital_total+=capital_mensual
                 tabla_amortizacion.append({
                     "mes": mes,
                     "saldo_pendiente": amor_pendiente,
